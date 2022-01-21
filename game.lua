56fa31c9ae155f8836dea1f5df20dfa6bec9e1ed
Disguised Fox <77709966+DisguisedFox94@users.noreply.github.com>
2022-01-20 22:50:15 -0600

    Update game.lua

--- game.lua
+++ game.lua
@@ -1,36 +1,7 @@
-local GameTable = {
-    ["2788229376"] = "dh",
-    ["1962086868"] = "toh"
-}
-
-function SendRequest(url)
-    local response = http_request(
-    {
-        Url = url,  -- This website helps debug HTTP requests
-        Method = "GET"
-    }
-    )
-    return response
+if game.PlaceId == 1962086868 then -- toh
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DisguisedFox94/CodeHub/main/toh.lua"))();
+elseif game.PlaceId == then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/DisguisedFox94/CodeHub/main/dahood.lua"))();
+else
+    game.Players.LocalPlayer:Kick('Unsupported game.')
 end
-
-local GameFound = false
-local ScriptData = nil
-
-
-
-local success, response = pcall(function()
-    for key, value in pairs(GameTable) do
-        if game.PlaceId == tonumber(key) then
-            GameFound = true
-            ScriptData = SendRequest("https://raw.githubusercontent.com/DisguisedFox/CodeHub/"..value..".lua")
-        end
-    end
-    if ScriptData and ScriptData.Body then
-        loadstring(ScriptData.Body)()
-    else
-        game.Players.LocalPlayer:Kick("\nSomething isnt right")
-        wait(2)
-        while true do end
-    end
-
-end)
