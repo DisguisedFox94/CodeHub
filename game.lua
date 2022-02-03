d3e6cdfc0735a3b4d6bd025c67e534f52b9463d7
Disguised Fox <77709966+DisguisedFox94@users.noreply.github.com>
2022-02-03 16:56:26 -0600

    Update game.lua

--- game.lua
+++ game.lua
@@ -4,6 +4,8 @@ elseif game.PlaceId == 2788229376 then
    loadstring(game:HttpGet("https://raw.githubusercontent.com/DisguisedFox94/CodeHub/main/dahood.lua"))();
 elseif game.PlaceId == 7993293100 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/DisguisedFox94/CodeHub/main/tsunami.lua"))();
+elseif game.PlaceId == 482742811 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DisguisedFox94/CodeHub/main/getcrushed.lua"))();
 else
     game.Players.LocalPlayer:Kick('Unsupported game.')
 end
