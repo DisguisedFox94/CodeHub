6aa2cea1f221e7eaf7748637daea673e97d7c637
Disguised Fox <77709966+DisguisedFox94@users.noreply.github.com>
2022-02-04 19:05:59 -0600

    Update game.lua

--- game.lua
+++ game.lua
@@ -74,7 +74,7 @@ elseif game.PlaceId == 482742811 then
     Notify("Code Hub", "Loaded: GET CRUSHED", 5)
 elseif game.PlaceId == 185655149 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/DisguisedFox94/CodeHub/main/wtb.lua"))();
-    Notify("Code Hub", "Loaded: GET CRUSHED", 5)
+    Notify("Code Hub", "Loaded: Welcome to Bloxburg", 5)
 else
     Notify("Code Hub", "Error: Unsupported game.", 5)
 end
